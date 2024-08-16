28c71d3be8d41193d706be501adc452f65ef2d30
Elizabeth Bond <104878451+ebond2633@users.noreply.github.com>
2024-08-16 13:07:01 -0400

    glab309.7.1

---
+++ src/main/java/com/RTT/_971/springCruddDemo/SpringCruddDemoApplication.java
@@ -0,0 +1,13 @@
+package com.RTT._971.springCruddDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages ="com.RTT._971.springCruddDemo" )
+public class SpringCruddDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCruddDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RTT/_971/springCruddDemo/SpringCruddDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RTT._971.springCruddDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCruddDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
